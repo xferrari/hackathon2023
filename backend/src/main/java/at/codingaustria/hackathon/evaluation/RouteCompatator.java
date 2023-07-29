1079543955ce8e6a178d325aa6310103258274a2
Hannes Marcher <hannes.marcher@tuwien.ac.at>
2023-07-29 22:42:01 +0200

    fix bug

--- backend/src/main/java/at/codingaustria/hackathon/evaluation/RouteCompatator.java
+++ backend/src/main/java/at/codingaustria/hackathon/evaluation/RouteCompatator.java
@@ -9,36 +9,30 @@ import java.util.List;
 
 public class RouteCompatator {
     public static Route compareRoutes(List<Location> locations1, List<Location> locations2) throws RouteNotFoundException, ApiException {
-        List<Location> result = new ArrayList<>();
-        Location locTest = locations1.get(0);
-        var additionalCosts = compareLocation(locations2.get(0), locTest);
-        ArrayList<Location> tempList = new ArrayList<>(locations1);
-        tempList.remove(0);
-        result.add(locTest);
-        var route = RouteCompatator.compareRoutes(tempList, new ArrayList<>(locations2), locTest);
-        result.addAll(route.getTargets());
-        return new Route(
-            result,
-            additionalCosts + route.getCosts()
-        );
+        locations1 = new ArrayList<>(locations1);
+        locations2 = new ArrayList<>(locations2);
+        Location curLocation = locations1.get(0);
+        locations1.remove(0);
+        return compareRoutes(locations1, locations2, curLocation);
     }
 
     public static Route compareRoutes(List<Location> locations1, List<Location> locations2, Location curLocation) throws RouteNotFoundException, ApiException {
         List<Location> result = new ArrayList<>();
         if (locations1.isEmpty() && locations2.isEmpty()) {
+            result.add(curLocation);
             return new Route(result, 0);
         } else if (locations1.isEmpty()) {
             var additionalCosts = compareLocation(locations2.get(0), curLocation);
-            curLocation = locations2.get(0);
             result.add(curLocation);
+            curLocation = locations2.get(0);
             locations2.remove(0);
             var route = compareRoutes(locations1, locations2, curLocation);
             result.addAll(route.getTargets());
             return new Route(result, route.getCosts() + additionalCosts);
         } else if (locations2.isEmpty()) {
             var additionalCosts = compareLocation(locations1.get(0), curLocation);
-            curLocation = locations1.get(0);
             result.add(curLocation);
+            curLocation = locations1.get(0);
             locations1.remove(0);
             var route = compareRoutes(locations1, locations2, curLocation);
             result.addAll(route.getTargets());
@@ -48,15 +42,15 @@ public class RouteCompatator {
         double additionalCosts1 = compareLocation(locations1.get(0), curLocation);
         double additionalCosts2 = compareLocation(locations2.get(0), curLocation);
         if (additionalCosts1 < additionalCosts2) {
-            curLocation = locations1.get(0);
             result.add(curLocation);
+            curLocation = locations1.get(0);
             locations2.remove(0);
             var route = compareRoutes(locations1, locations2, curLocation);
             result.addAll(route.getTargets());
             return new Route(result, route.getCosts() + additionalCosts1);
         }
-        curLocation = locations2.get(0);
         result.add(curLocation);
+        curLocation = locations2.get(0);
         locations2.remove(0);
         var route = compareRoutes(locations1, locations2, curLocation);
         result.addAll(route.getTargets());
