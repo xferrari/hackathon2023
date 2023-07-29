0815a3e78d5ba7e043e5d486be00d9cce7a5e60c
robert.gludovatz <r.gludovatz@ntbs.at>
2023-07-29 20:18:30 +0200

    removed path, removed calcPoints Flag, removed allPoints

--- backend/src/main/java/at/codingaustria/hackathon/obj/Route.java
+++ backend/src/main/java/at/codingaustria/hackathon/obj/Route.java
@@ -4,26 +4,18 @@ import java.util.List;
 
 public class Route {
     List<Location> targets;
-    List<Location> path;
     double costs;
 
-    public Route(List<Location> targets, List<Location> path, double costs) {
+    public Route(List<Location> targets, double costs) {
         this.targets = targets;
-        this.path = path;
         this.costs = costs;
     }
 
-    public Route() {
-
-    }
 
     public List<Location> getTargets() {
         return targets;
     }
 
-    public List<Location> getPath() {
-        return path;
-    }
 
     public double getCosts() {
         return costs;
