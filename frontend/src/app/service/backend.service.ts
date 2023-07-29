ab18978e730e178af8500092548696c1845ae22c
christopherderinger <christopher.deringer@gmail.com>
2023-07-29 22:18:56 +0200

    feat: Routes are drawn based on api res

--- frontend/src/app/service/backend.service.ts
+++ frontend/src/app/service/backend.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Route } from '@angular/router';
 import { Res } from '../model/res.model';
+import { Route } from '../model/route.model';
 
 @Injectable()
 export class BackendService {
   constructor(private http: HttpClient) {}
 
   getRoutes() {
-    let url = 'http://localhost:8080/api/initial';
+    let url = 'http://localhost:8080/api/initial2';
     return this.http.get<[Route]>(url);
   }
 }
