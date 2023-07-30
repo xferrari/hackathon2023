f72d4ef91498b82afa6f1731cd01767e8f7adb45
robert.gludovatz <r.gludovatz@ntbs.at>
2023-07-30 09:38:31 +0200

    updated initial data. added names to initdata1

--- backend/src/main/java/at/codingaustria/hackathon/controller/RouteContoller.java
+++ backend/src/main/java/at/codingaustria/hackathon/controller/RouteContoller.java
@@ -55,28 +55,43 @@ public class RouteContoller {
     @ResponseBody
     public ResponseEntity<List<Route>> getInitData() throws RouteNotFoundException, ApiException {
         List<Location> list1 = new ArrayList<>();
-        list1.add(new Location(48.21612, 16.373137));
-        list1.add(new Location(48.4017869, 15.9847379));
-        list1.add(new Location(48.3739927, 16.6357775));
-        list1.add(new Location(48.1245335, 14.8823511));
+        list1.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list1.add(new Location(48.527794, 16.3619866, "Raiffeisenkasse Ernstbrunn eGen"));
         Route route1 = RouteEvaluator.getFullRouteInformation(list1);
 
         List<Location> list2 = new ArrayList<>();
-        list2.add(new Location(48.4017869, 15.9847379));
-        list2.add(new Location(48.3739927, 16.6357775));
-        list2.add(new Location(48.1245335, 14.8823511));
+        list2.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list2.add(new Location(48.0863213, 16.291321, "Raiffeisen Regionalbank Mödling eGen"));
         Route route2 = RouteEvaluator.getFullRouteInformation(list2);
 
         List<Location> list3 = new ArrayList<>();
-        list3.add(new Location(48.0053016, 16.231961));
-        list3.add(new Location(48.4017869, 15.9847379));
-        list3.add(new Location(48.1245335, 14.8823511));
+        list3.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list3.add(new Location(48.56526, 16.0799974, "Raiffeisen Regionalbank Mödling eGen"));
         Route route3 = RouteEvaluator.getFullRouteInformation(list3);
+
+        List<Location> list4 = new ArrayList<>();
+        list4.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list4.add(new Location(48.3738038, 16.3137991, "Raiffeisenbank Korneuburg eGen"));
+        Route route4 = RouteEvaluator.getFullRouteInformation(list4);
+
+        List<Location> list5 = new ArrayList<>();
+        list5.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list5.add(new Location(48.1464987, 16.7032167, "Raiffeisenkasse Orth a.d. Donau eGen"));
+        Route route5 = RouteEvaluator.getFullRouteInformation(list5);
+
+        List<Location> list6 = new ArrayList<>();
+        list6.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list6.add(new Location(48.1784607, 16.0761195, "Raiffeisenbank Wienerwald eGen"));
+        Route route6 = RouteEvaluator.getFullRouteInformation(list6);
+
 
         List<Route> listOfTheList = new ArrayList<>();
         listOfTheList.add(route1);
         listOfTheList.add(route2);
         listOfTheList.add(route3);
+        listOfTheList.add(route4);
+        listOfTheList.add(route5);
+        listOfTheList.add(route6);
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
@@ -88,29 +103,29 @@ public class RouteContoller {
     @ResponseBody
     public ResponseEntity<List<Route>> getInitData2() throws RouteNotFoundException, ApiException {
         List<Location> list2 = new ArrayList<>();
-        list2.add(new Location(48.4017869, 15.9847379,"Raiffeisenbank Region Wagram eGen"));
-        list2.add(new Location(48.3739927, 16.6357775,"Raiffeisenbank Auersthal-Bockfließ-Groß Schweinbarth eGen"));
-        list2.add(new Location(48.1245335, 14.8823511,"Raiffeisenbank Region Amstetten eGen"));
+        list2.add(new Location(48.4017869, 15.9847379, "Raiffeisenbank Region Wagram eGen"));
+        list2.add(new Location(48.3739927, 16.6357775, "Raiffeisenbank Auersthal-Bockfließ-Groß Schweinbarth eGen"));
+        list2.add(new Location(48.1245335, 14.8823511, "Raiffeisenbank Region Amstetten eGen"));
         Route route2 = RouteEvaluator.getFullRouteInformation(list2);
 
         List<Location> list3 = new ArrayList<>();
-        list3.add(new Location(48.0053016, 16.231961,"Raiffeisenbank Region Baden eGen"));
-        list3.add(new Location(48.4017869, 15.9847379,"Raiffeisenbank Region Wagram eGen"));
-        list3.add(new Location(48.1245335, 14.8823511,"Raiffeisenbank Region Amstetten eGen"));
+        list3.add(new Location(48.0053016, 16.231961, "Raiffeisenbank Region Baden eGen"));
+        list3.add(new Location(48.4017869, 15.9847379, "Raiffeisenbank Region Wagram eGen"));
+        list3.add(new Location(48.1245335, 14.8823511, "Raiffeisenbank Region Amstetten eGen"));
         Route route3 = RouteEvaluator.getFullRouteInformation(list3);
 
         List<Location> list1 = new ArrayList<>();
-        list1.add(new Location(48.21612, 16.373137,"RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
-        list1.add(new Location(48.4017869, 15.9847379,"Raiffeisenbank Region Wagram eGen"));
-        list1.add(new Location(48.3739927, 16.6357775,"Raiffeisenbank Auersthal-Bockfließ-Groß Schweinbarth eGen"));
-        list1.add(new Location(48.1245335, 14.8823511,"Raiffeisenbank Region Amstetten eGen"));
+        list1.add(new Location(48.21612, 16.373137, "RAIFFEISENLANDESBANK NIEDERÖSTERREICH-WIEN AG"));
+        list1.add(new Location(48.4017869, 15.9847379, "Raiffeisenbank Region Wagram eGen"));
+        list1.add(new Location(48.3739927, 16.6357775, "Raiffeisenbank Auersthal-Bockfließ-Groß Schweinbarth eGen"));
+        list1.add(new Location(48.1245335, 14.8823511, "Raiffeisenbank Region Amstetten eGen"));
         Route route1 = RouteEvaluator.getFullRouteInformation(list1);
 
         List<Location> list4 = new ArrayList<>();
-        list4.add(new Location(47.845516, 16.516668,"Raiffeisenlandesbank Burgenland und Revisionsverband eGen"));
-        list4.add(new Location(47.7766024, 17.0326694,"Raiffeisenbank Seewinkel-Hansag eGen"));
-        list4.add(new Location(47.6000949, 16.6252523,"Raiffeisenbank Region Deutschkreutz-Horitschon eGen"));
-        list4.add(new Location(47.8966677, 16.6438987,"Raiffeisenbank Neusiedlersee-HÃ¼gelland eGen"));
+        list4.add(new Location(47.845516, 16.516668, "Raiffeisenlandesbank Burgenland und Revisionsverband eGen"));
+        list4.add(new Location(47.7766024, 17.0326694, "Raiffeisenbank Seewinkel-Hansag eGen"));
+        list4.add(new Location(47.6000949, 16.6252523, "Raiffeisenbank Region Deutschkreutz-Horitschon eGen"));
+        list4.add(new Location(47.8966677, 16.6438987, "Raiffeisenbank Neusiedlersee-HÃ¼gelland eGen"));
         Route route4 = RouteEvaluator.getFullRouteInformation(list4);
 
         List<Route> listOfTheList = new ArrayList<>();
@@ -136,14 +151,6 @@ public class RouteContoller {
         return null;
     }
 
-    private List<Location> getAllLocations(List<Route> routes) {
-        var allLocations = new ArrayList<Location>();
-        for (var route : routes) {
-            allLocations.addAll(route.getTargets());
-        }
-        return allLocations;
-    }
-
     @PostMapping("api/mergeRoutes")
     public ResponseEntity<List<Route>> mergeRoutes(@RequestBody List<Route> routes)
             throws RouteNotFoundException, ApiException {
@@ -156,7 +163,6 @@ public class RouteContoller {
         }
 
 
-
         var size = 0;
         var counter = 0;
         do {
@@ -166,6 +172,14 @@ public class RouteContoller {
         } while (size > routesWithCosts.size());
 
         return new ResponseEntity<>(routesWithCosts, HttpStatus.OK);
+    }
+
+    private List<Location> getAllLocations(List<Route> routes) {
+        var allLocations = new ArrayList<Location>();
+        for (var route : routes) {
+            allLocations.addAll(route.getTargets());
+        }
+        return allLocations;
     }
 
     private void mergeRoutes1(List<Route> routes, List<Location> initial) throws RouteNotFoundException, ApiException {
