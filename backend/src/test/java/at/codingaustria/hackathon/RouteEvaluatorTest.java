a81a42b90481337240b52af02b39af50f0635274
Hannes Marcher <hannes.marcher@tuwien.ac.at>
2023-07-30 09:25:49 +0200

    add test for distance with stop

--- backend/src/test/java/at/codingaustria/hackathon/RouteEvaluatorTest.java
+++ backend/src/test/java/at/codingaustria/hackathon/RouteEvaluatorTest.java
@@ -10,15 +10,29 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class RouteEvaluatorTest {
+
   @Test
   public void test() throws RouteNotFoundException, ApiException {
     var locations = List.of(
-        new Location(48.2082, 16.3738),
-        new Location(47.2692, 11.4041)
+        new Location(48.2082, 16.3738), //Wien
+        new Location(47.2692, 11.4041) //Innsbruck
     );
 
     assertEquals(
-        476282.807,RouteEvaluator.getFullRouteInformation(locations).getCosts()
+        476282.807, RouteEvaluator.getFullRouteInformation(locations).getCosts()
+    );
+  }
+
+  @Test
+  public void test_with_stop() throws RouteNotFoundException, ApiException {
+    var locations = List.of(
+        new Location(48.2082, 16.3738), //Wien
+        new Location(47.2692, 11.4041), //Innsbruck
+        new Location(46.6167, 14.3000) //Klagenfurt
+    );
+
+    assertEquals(
+        795842.042, RouteEvaluator.getFullRouteInformation(locations).getCosts()
     );
   }
 }
