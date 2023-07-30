18fa83f8ec511afcee2fe321c0e3b7bc864bb2ed
Hannes Marcher <hannes.marcher@tuwien.ac.at>
2023-07-30 09:57:38 +0200

    add openapi endpoint

--- backend/src/main/java/at/codingaustria/hackathon/Application.java
+++ backend/src/main/java/at/codingaustria/hackathon/Application.java
@@ -2,11 +2,38 @@ package at.codingaustria.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("at.codingaustria.hackathon"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Your API Title")
+            .description("Your API Description")
+            .version("1.0.0")
+            .build();
     }
 }
